1f3f0cc5e94568ec1a0a07bc1fd5710a82599734
Chracken <55137336+Chracken@users.noreply.github.com>
2020-11-21 00:08:37 +0200

    Update bot.sh

--- Bot Files/bot.sh
+++ Bot Files/bot.sh
@@ -3,7 +3,9 @@ echo ""
 cd "$root"
 pip3 install --upgrade pip &
 pip3 install -r dependencies.txt &
+clear
+clear
 sleep 3
 clear
-python3.6 bot.py &
-exit 0
+echo "Check Bot Status on Discord"
+python3.6 bot.py
